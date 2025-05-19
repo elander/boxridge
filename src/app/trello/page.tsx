590c88be3eb1319ff850d318920d916ff1e8f140
Max Elander <max@maxelander.se>
2025-05-19 15:39:03 +0200

    nytt försök

--- src/app/trello/page.tsx
+++ src/app/trello/page.tsx
@@ -1,7 +1,7 @@
 
 
 export default function Trello() {
-const initialData ={
+const laneData ={
   active:0,
   lanes:[
     {
@@ -19,7 +19,43 @@ const initialData ={
     return (
         <div>
             <h1>Trello</h1>
-            
+            <div>
+        {// DndContext here
+        laneData.lanes.map((lane) => (
+          <Lane key={lane.name} lane={lane} />
+          ))}
+    </div>
         </div>
     );
 }
+
+
+type LaneProps = {
+    lane: {
+        name: string;
+        bgcolor: string;
+        list: {
+            id: string;
+            title: string;
+            cards: {
+                id: string;
+                title: string;
+            }[];
+        }[];
+    };
+};
+
+ function Lane(props: LaneProps) {
+    return (
+        <div key={props.lane.name} className="p-4">
+            <h2 className="text-2xl font-bold mb-2">{props.lane.name}</h2>
+            <div className="grid grid-cols-5 gap-4">
+              {props.lane.list.map((list) => (
+                <div id={list.id} key={list.id} className="bg-white p-4 rounded shadow text-gray-800">
+                  Lista
+                </div>
+                ))}
+            </div>
+        </div>
+    );
+ }
